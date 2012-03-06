b9b1dffd3f3d4dfa59d3b520f7605ab24df250d5
Willem-Jan.Boogerd@kb.nl <Willem-Jan.Boogerd@kb.nl>
2012-03-06 12:00:25 +0000

    API2: supporting default images

---
+++ src/main/java/eu/europeana/api2/utils/DefaultImageCache.java
@@ -0,0 +1,17 @@
+package eu.europeana.api2.utils;
+
+import eu.europeana.corelib.definitions.solr.DocType;
+
+public class DefaultImageCache {
+
+	public static byte[] getImage(DocType type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static byte[] getImageUnknown() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/eu/europeana/api2/web/controller/ImageController.java
+++ src/main/java/eu/europeana/api2/web/controller/ImageController.java
@@ -27,27 +27,61 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import eu.europeana.api2.utils.DefaultImageCache;
+import eu.europeana.corelib.db.exception.DatabaseException;
 import eu.europeana.corelib.db.service.ThumbnailService;
 import eu.europeana.corelib.definitions.model.ThumbSize;
+import eu.europeana.corelib.definitions.solr.beans.FullBean;
+import eu.europeana.corelib.solr.exceptions.SolrTypeException;
+import eu.europeana.corelib.solr.service.SearchService;
 
 /**
  * @author Willem-Jan Boogerd <www.eledge.net/contact>
  */
 @Controller
 public class ImageController {
-	
+
 	@Resource
-	ThumbnailService thumbnailService;
-	
-	@RequestMapping(value="/image", produces=MediaType.IMAGE_JPEG_VALUE)
-	public ResponseEntity<byte[]> image(
-			@RequestParam(value="apikey", required=true) String apiKey,
-			@RequestParam(value="objectId", required=true) String objectid,
-			@RequestParam(value="size", required=false, defaultValue="MEDIUM") ThumbSize size
-	) {
-		byte[] image = thumbnailService.retrieveThumbnail(objectid, size);
+	private ThumbnailService thumbnailService;
+
+	@Resource
+	private SearchService searchService;
+
+	@RequestMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<byte[]> image(@RequestParam(value = "apikey", required = true) String apiKey,
+			@RequestParam(value = "objectId", required = true) String objectid,
+			@RequestParam(value = "size", required = false, defaultValue = "MEDIUM") ThumbSize size) {
+		// TODO: apikey checking
+		byte[] image = null;
+		MediaType mediaType = MediaType.IMAGE_JPEG;
+		try {
+			if (thumbnailService.exists(objectid)) {
+				image = thumbnailService.retrieveThumbnail(objectid, size);
+			}
+		} catch (DatabaseException e) {
+			// ignore and return default image
+		}
+		if (image == null) {
+			// retrieve record
+			FullBean bean;
+			try {
+				bean = searchService.findById(objectid);
+				if (bean != null) {
+					image = DefaultImageCache.getImage(bean.getType());
+					mediaType = MediaType.IMAGE_GIF;
+				}
+			} catch (SolrTypeException e) {
+				// ignore and image for unknown
+			}
+		}
+		if (image == null) {
+			// load image for unkown type/object
+			image = DefaultImageCache.getImageUnknown();
+			mediaType = MediaType.IMAGE_PNG;
+		}
+
 		final HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.IMAGE_JPEG);
+		headers.setContentType(mediaType);
 		return new ResponseEntity<byte[]>(image, headers, HttpStatus.OK);
 	}
 
