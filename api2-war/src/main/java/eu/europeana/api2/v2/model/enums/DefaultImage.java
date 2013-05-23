c5ba5c1c74f98d026c4e1509afd42e0cf438ccd8
peter.kiraly@kb.nl <peter.kiraly@kb.nl>
2013-05-23 13:59:33 +0000

    Code format change

--- api2-war/src/main/java/eu/europeana/api2/v2/model/enums/DefaultImage.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/model/enums/DefaultImage.java
@@ -11,22 +11,22 @@ import eu.europeana.corelib.utils.ImageUtils;
 
 public enum DefaultImage {
 
-	TT(ThumbSize.TINY, DocType.TEXT, "/images/item-text-tiny.gif"), TI(
-			ThumbSize.TINY, DocType.IMAGE, "/images/item-image-tiny.gif"), TS(
-			ThumbSize.TINY, DocType.SOUND, "/images/item-sound-tiny.gif"), TV(
-			ThumbSize.TINY, DocType.VIDEO, "/images/item-video-tiny.gif"), T3(
-			ThumbSize.TINY, DocType._3D, "/images/item-3d-tiny.gif"), MT(
-			ThumbSize.MEDIUM, DocType.TEXT, "/images/item-text.gif"), MI(
-			ThumbSize.MEDIUM, DocType.IMAGE, "/images/item-image.gif"), MS(
-			ThumbSize.MEDIUM, DocType.SOUND, "/images/item-sound.gif"), MV(
-			ThumbSize.MEDIUM, DocType.VIDEO, "/images/item-video.gif"), M3(
-			ThumbSize.MEDIUM, DocType._3D, "/images/item-3d.gif"), LT(
-			ThumbSize.LARGE, DocType.TEXT, "/images/item-text-large.gif"), LI(
-			ThumbSize.LARGE, DocType.IMAGE, "/images/item-image-large.gif"), LS(
-			ThumbSize.LARGE, DocType.SOUND, "/images/item-sound-large.gif"), LV(
-			ThumbSize.LARGE, DocType.VIDEO, "/images/item-video-large.gif"), L3(
-			ThumbSize.LARGE, DocType._3D, "/images/item-3d-large.gif"), UNKNOWN(
-			null, null, "/images/unknown.png");
+	TT(ThumbSize.TINY, DocType.TEXT, "/images/item-text-tiny.gif"),
+	TI(ThumbSize.TINY, DocType.IMAGE, "/images/item-image-tiny.gif"),
+	TS(ThumbSize.TINY, DocType.SOUND, "/images/item-sound-tiny.gif"),
+	TV(ThumbSize.TINY, DocType.VIDEO, "/images/item-video-tiny.gif"),
+	T3(ThumbSize.TINY, DocType._3D, "/images/item-3d-tiny.gif"),
+	MT(ThumbSize.MEDIUM, DocType.TEXT, "/images/item-text.gif"),
+	MI(ThumbSize.MEDIUM, DocType.IMAGE, "/images/item-image.gif"),
+	MS(ThumbSize.MEDIUM, DocType.SOUND, "/images/item-sound.gif"),
+	MV(ThumbSize.MEDIUM, DocType.VIDEO, "/images/item-video.gif"),
+	M3(ThumbSize.MEDIUM, DocType._3D, "/images/item-3d.gif"),
+	LT(ThumbSize.LARGE, DocType.TEXT, "/images/item-text-large.gif"),
+	LI(ThumbSize.LARGE, DocType.IMAGE, "/images/item-image-large.gif"),
+	LS(ThumbSize.LARGE, DocType.SOUND, "/images/item-sound-large.gif"),
+	LV(ThumbSize.LARGE, DocType.VIDEO, "/images/item-video-large.gif"),
+	L3(ThumbSize.LARGE, DocType._3D, "/images/item-3d-large.gif"), 
+	UNKNOWN(null, null, "/images/unknown.png");
 
 	private ThumbSize size;
 	private DocType type;
@@ -62,5 +62,4 @@ public enum DefaultImage {
 	public byte[] getCache() {
 		return cache;
 	}
-
 }
