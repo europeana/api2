bd4b1d88bdcd0ac6c8db11a13b6001d34387772e
wjboogerd <wjboogerd@eledge.net>
2013-10-14 14:08:45 +0200

    Re #410 - Fixed tag deletion

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/user/UserTagController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/user/UserTagController.java
@@ -46,14 +46,16 @@ import eu.europeana.corelib.definitions.db.entity.relational.User;
 public class UserTagController extends AbstractUserController {
 
 	@RequestMapping(value = "/v2/user/tag.json", params = "!action", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ModelAndView defaultAction(@RequestParam(value = "europeanaid", required = false) String europeanaId,
+	public ModelAndView defaultAction(
+			@RequestParam(value = "europeanaid", required = false) String europeanaId,
 			@RequestParam(value = "tag", required = false) String tag,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		return list(europeanaId, tag, callback, principal);
 	}
 
 	@RequestMapping(value = "/v2/user/tag.json", params = "action=LIST", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ModelAndView list(@RequestParam(value = "europeanaid", required = false) String europeanaId,
+	public ModelAndView list(
+			@RequestParam(value = "europeanaid", required = false) String europeanaId,
 			@RequestParam(value = "tag", required = false) String tagFilter,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		User user = userService.findByEmail(principal.getName());
@@ -87,7 +89,8 @@ public class UserTagController extends AbstractUserController {
 	}
 
 	@RequestMapping(value = "/v2/user/tag.json", params = "action=TAGCLOUD", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ModelAndView listDistinct(@RequestParam(value = "callback", required = false) String callback,
+	public ModelAndView listDistinct(
+			@RequestParam(value = "callback", required = false) String callback,
 			Principal principal) {
 		User user = userService.findByEmail(principal.getName());
 		if (user != null) {
@@ -108,14 +111,16 @@ public class UserTagController extends AbstractUserController {
 
 	@RequestMapping(value = "/v2/user/tag.json", produces = MediaType.APPLICATION_JSON_VALUE, method = {
 			RequestMethod.POST, RequestMethod.PUT })
-	public ModelAndView createRest(@RequestParam(value = "europeanaid", required = true) String europeanaId,
+	public ModelAndView createRest(
+			@RequestParam(value = "europeanaid", required = true) String europeanaId,
 			@RequestParam(value = "tag", required = true) String tag,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		return create(europeanaId, tag, callback, principal);
 	}
 
 	@RequestMapping(value = "/v2/user/tag.json", params = "action=CREATE", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ModelAndView create(@RequestParam(value = "europeanaid", required = true) String europeanaId,
+	public ModelAndView create(
+			@RequestParam(value = "europeanaid", required = true) String europeanaId,
 			@RequestParam(value = "tag", required = true) String tag,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		User user = userService.findByEmail(principal.getName());
@@ -133,17 +138,19 @@ public class UserTagController extends AbstractUserController {
 	}
 
 	@RequestMapping(value = "/v2/user/tag.json", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.DELETE)
-	public ModelAndView deleteRest(@RequestParam(value = "tagid", required = false) Long tagId,
-			@RequestParam(value = "tag", required = true) String tag,
-			@RequestParam(value = "europeanaid", required = true) String europeanaId,
+	public ModelAndView deleteRest(
+			@RequestParam(value = "tagid", required = false) Long tagId,
+			@RequestParam(value = "tag", required = false) String tag,
+			@RequestParam(value = "europeanaid", required = false) String europeanaId,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		return delete(tagId, tag, europeanaId, callback, principal);
 	}
 
 	@RequestMapping(value = "/v2/user/tag.json", params = "action=DELETE", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ModelAndView delete(@RequestParam(value = "tagid", required = false) Long tagId,
-			@RequestParam(value = "tag", required = true) String tag,
-			@RequestParam(value = "europeanaid", required = true) String europeanaId,
+	public ModelAndView delete(
+			@RequestParam(value = "tagid", required = false) Long tagId,
+			@RequestParam(value = "tag", required = false) String tag,
+			@RequestParam(value = "europeanaid", required = false) String europeanaId,
 			@RequestParam(value = "callback", required = false) String callback, Principal principal) {
 		User user = userService.findByEmail(principal.getName());
 		UserModification response = new UserModification(getApiId(principal), "/v2/user/tag.json?action=DELETE");
