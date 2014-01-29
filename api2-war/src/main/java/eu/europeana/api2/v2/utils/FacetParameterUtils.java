7c54a22e55c58ff6f5c605f2224c4e0831a99a2a
Kiraly Peter <peter.kiraly@kb.nl>
2014-01-29 13:55:51 +0100

    Re #1152 Adding tests and documentations.

--- api2-war/src/main/java/eu/europeana/api2/v2/utils/FacetParameterUtils.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/utils/FacetParameterUtils.java
@@ -8,31 +8,62 @@ import org.apache.commons.lang.ArrayUtils;
 import eu.europeana.api2.v2.model.NumericFacetParameter;
 import eu.europeana.corelib.definitions.solr.Facet;
 
+/**
+ * Utility class for extracting numeric parameters specific to a given facet.
+ * 
+ * @author Peter.Kiraly@kb.nl
+ */
 public class FacetParameterUtils {
 
 	final static String DEFAULT_LIMIT_KEY = "f.DEFAULT.facet.limit";
 	final static String DEFAULT_OFFSET_KEY = "f.DEFAULT.facet.offset";
 
-
+	/**
+	 * Returns all relevant parameters of a given type (right now: limit and offset)
+	 * 
+	 * @param type
+	 *   Type of parameter. Currently we support "limit" and "offset"
+	 * @param facets
+	 *   List of facets
+	 * @param parameters
+	 *   The request parameters
+	 * @param isDefaultFacetsRequested
+	 *   Whether or not the default facets should be checked
+	 * @return
+	 */
 	public static Map<String, Integer> getFacetParams(String type, String[] facets, 
 			Map<Object, Object> parameters,
 			boolean isDefaultFacetsRequested) {
 		Map<String, Integer> facetParams = new HashMap<String, Integer>();
 		if (isDefaultFacetsRequested) {
 			for (Facet facet : Facet.values()) {
-				getFacetParam(type, facet.name(), parameters, true, facetParams);
+				saveFacetParam(type, facet.name(), parameters, true, facetParams);
 			}
 		}
 
 		if (ArrayUtils.isNotEmpty(facets)) {
 			for (String facet : facets) {
-				getFacetParam(type, facet, parameters, false, facetParams);
+				saveFacetParam(type, facet, parameters, false, facetParams);
 			}
 		}
 		return facetParams;
 	}
 
-	public static void getFacetParam(String type, String name, Map<Object, Object> parameters, 
+	/**
+	 * Extracts and saves parameter of a given type (right now: limit and offset) belongs to a facet
+	 * 
+	 * @param type
+	 *   The type of parameter (limit or offset)
+	 * @param name
+	 *   The name of the facet
+	 * @param parameters
+	 *   Request parameters
+	 * @param isDefault
+	 *   The facet is a default facet
+	 * @param facetParams
+	 *   The container to save into
+	 */
+	public static void saveFacetParam(String type, String name, Map<Object, Object> parameters, 
 			boolean isDefault, Map<String, Integer> facetParams) {
 		NumericFacetParameter parameter = null;
 		if (type.equals("limit")) {
