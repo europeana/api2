d2d490d1214ea6896ebf7f66a3d1d2141a979033
Kiraly Peter <peter.kiraly@kb.nl>
2014-05-08 11:41:59 +0200

    Re #1424 BUG: Some IsShownAt links from the API not resolving
     * Fixing isShownAt link encodings

--- api2-model/src/main/java/eu/europeana/api2/model/utils/LinkUtils.java
+++ api2-model/src/main/java/eu/europeana/api2/model/utils/LinkUtils.java
@@ -1,10 +1,5 @@
 package eu.europeana.api2.model.utils;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import org.apache.commons.lang.StringUtils;
-
 import eu.europeana.corelib.web.utils.UrlBuilder;
 
 public class LinkUtils {
@@ -14,17 +9,5 @@ public class LinkUtils {
 		url.addParam("utm_medium", "api");
 		url.addParam("utm_campaign", wskey);
 		return url.toString();
-	}
-
-	public static String encode(String value) {
-		if (StringUtils.isBlank(value)) {
-			return "";
-		}
-		try {
-			value = URLEncoder.encode(value, "utf-8");
-		} catch (UnsupportedEncodingException e) {
-			// ignore, won't happen normally
-		}
-		return value;
 	}
 }
