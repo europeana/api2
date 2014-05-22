65d3328d4de92ca999e28f1279bd97ac90737199
Kiraly Peter <peter.kiraly@kb.nl>
2014-05-22 16:33:26 +0200

    Re #1471 New API call: /v2/translateQuery.json

---
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/QueryTranslationController.java
@@ -0,0 +1,85 @@
+package eu.europeana.api2.v2.web.controller;
+
+import java.util.Arrays;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import eu.europeana.api2.model.enums.ApiLimitException;
+import eu.europeana.api2.model.json.ApiError;
+import eu.europeana.api2.utils.JsonUtils;
+import eu.europeana.api2.v2.model.LimitResponse;
+import eu.europeana.api2.v2.model.json.QueryTranslationResult;
+import eu.europeana.api2.v2.utils.ControllerUtils;
+import eu.europeana.corelib.db.entity.enums.RecordType;
+import eu.europeana.corelib.definitions.solr.model.Query;
+import eu.europeana.corelib.solr.utils.SolrUtils;
+import eu.europeana.corelib.utils.StringArrayUtils;
+
+@Controller
+public class QueryTranslationController {
+
+	@Resource
+	private ControllerUtils controllerUtils;
+
+	private static final String ERROR_TERM = "Invalid parameter: term can not be empty";
+	private static final String ERROR_LANGUAGE = "Invalid parameter: languageCodes can not be empty";
+
+	@RequestMapping(value = "/v2/translateQuery.json", method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ModelAndView translateQuery(
+			@RequestParam(value = "term", required = true) String term,
+			@RequestParam(value = "languageCodes", required = true) String[] languageCodes,
+			@RequestParam(value = "wskey", required = true) String wskey,
+			@RequestParam(value = "profile", required = false) String profile,
+			@RequestParam(value = "callback", required = false) String callback,
+			HttpServletRequest request,
+			HttpServletResponse response) {
+		response.setCharacterEncoding("UTF-8");
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		response.addHeader("Access-Control-Allow-Methods", "POST");
+		response.addHeader("Access-Control-Max-Age", "1000");
+
+		languageCodes = StringArrayUtils.splitWebParameter(languageCodes);
+
+		LimitResponse limitResponse = null;
+		try {
+			limitResponse = controllerUtils.checkLimit(wskey, request.getRequestURL().toString(),
+					"translateQuery.json", RecordType.TRANSLATE_QUERY, null);
+		} catch (ApiLimitException e) {
+			response.setStatus(e.getHttpStatus());
+			return JsonUtils.toJson(new ApiError(e), callback);
+		}
+
+		if (StringUtils.isBlank(term)) {
+			return JsonUtils.toJson(new ApiError(wskey, "translateQuery", ERROR_TERM, limitResponse.getRequestNumber()), callback);
+		} else if (StringArrayUtils.isBlank(languageCodes)) {
+			return JsonUtils.toJson(new ApiError(wskey, "translateQuery", ERROR_LANGUAGE, limitResponse.getRequestNumber()), callback);
+		}
+
+		QueryTranslationResult queryTranslationResult = 
+				new QueryTranslationResult(wskey, "translateQuery.json", limitResponse.getRequestNumber());
+
+		queryTranslationResult.translations = SolrUtils.translateQuery(term, Arrays.asList(languageCodes));
+		queryTranslationResult.translatedQuery = Query.concatenateQueryTranslations(queryTranslationResult.translations);
+
+		if (StringUtils.isNotBlank(profile) && StringUtils.containsIgnoreCase(profile, "param")) {
+			queryTranslationResult.addParam("wskey", wskey);
+			queryTranslationResult.addParam("term", term);
+			queryTranslationResult.addParam("languageCodes", languageCodes);
+			queryTranslationResult.addParam("profile", profile);
+			queryTranslationResult.addParam("callback", callback);
+		}
+
+		return JsonUtils.toJson(queryTranslationResult, callback);
+	}
+}
