d159b89a93a63c5784e182ac54ffdc8d2059b53b
bramfoo <bram.lohman@europeana.eu>
2014-11-13 16:33:36 +0100

    Added JavaDoc

--- api2-war/src/main/java/eu/europeana/api2/v2/utils/ControllerUtils.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/utils/ControllerUtils.java
@@ -13,57 +13,88 @@ import eu.europeana.corelib.db.service.ApiLogService;
 import eu.europeana.corelib.definitions.db.entity.relational.ApiKey;
 import eu.europeana.corelib.logging.Logger;
 
+/**
+ * Class containing a number of useful controller utilities
+ *
+ */
 public class ControllerUtils {
 
-	private Logger log = Logger.getLogger(ControllerUtils.class.getCanonicalName());
+  private Logger log = Logger.getLogger(ControllerUtils.class.getCanonicalName());
 
-	@Resource
-	private ApiKeyService apiService;
+  @Resource
+  private ApiKeyService apiService;
 
-	@Resource
-	private ApiLogService apiLogService;
+  @Resource
+  private ApiLogService apiLogService;
 
-	public LimitResponse checkLimit(String wskey, String url, String apiCall, RecordType recordType, String profile) 
-			throws ApiLimitException {
-		ApiKey apiKey = null;
-		long requestNumber = 0;
-		long t;
-		try {
-			t = System.currentTimeMillis();
-			apiKey = apiService.findByID(wskey);
-			if (apiKey == null) {
-				throw new ApiLimitException(wskey, apiCall, "Unregistered user", 0, 401);
-			}
-			// apiKey.getUsageLimit();
-			log.info("get apiKey: " + (System.currentTimeMillis() - t));
-			t = System.currentTimeMillis();
-			requestNumber = apiService.checkReachedLimit(apiKey);
-			log.info("checkReachedLimit: " + (System.currentTimeMillis() - t));
-			t = System.currentTimeMillis();
-			apiLogService.logApiRequest(wskey, url, recordType, profile);
-			log.info("logApiRequest: " + (System.currentTimeMillis() - t));
-		} catch (DatabaseException e) {
-			apiLogService.logApiRequest(wskey, url, recordType, profile);
-			throw new ApiLimitException(wskey, apiCall, e.getMessage(), requestNumber, 401);
-		} catch (LimitReachedException e) {
-			apiLogService.logApiRequest(wskey, url, RecordType.LIMIT, recordType.toString());
-			throw new ApiLimitException(wskey, apiCall, e.getMessage(), requestNumber, 429);
-		}
-		return new LimitResponse(apiKey, requestNumber);
-	}
+  /**
+   * Check the number requests made to the API in the last (rolling) 24 hours. This is a per-user
+   * limit (default: 10000) set in the myEuropeana database that is used as a throttling mechanism.
+   * 
+   * @param wskey The user's API web service key
+   * @param url The requested URL
+   * @param apiCall The REST endpoint of the call
+   * @param recordType The type of record, defined by {@link RecordType}
+   * @param profile The profile used
+   * @return A {@link LimitResponse} consisting of the apiKey and current request number
+   * @throws {@link ApiLimitException} if an unregistered or unauthorised key is provided, or if the
+   *         daily limit has been reached
+   */
+  public LimitResponse checkLimit(String wskey, String url, String apiCall, RecordType recordType,
+      String profile) throws ApiLimitException {
+    ApiKey apiKey = null;
+    long requestNumber = 0;
+    long t;
+    try {
+      t = System.currentTimeMillis();
+      apiKey = apiService.findByID(wskey);
+      if (apiKey == null) {
+        throw new ApiLimitException(wskey, apiCall, "Unregistered user", 0, 401);
+      }
+      // apiKey.getUsageLimit();
+      log.info("get apiKey: " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+      requestNumber = apiService.checkReachedLimit(apiKey);
+      log.info("checkReachedLimit: " + (System.currentTimeMillis() - t));
+      t = System.currentTimeMillis();
+      apiLogService.logApiRequest(wskey, url, recordType, profile);
+      log.info("logApiRequest: " + (System.currentTimeMillis() - t));
+    } catch (DatabaseException e) {
+      apiLogService.logApiRequest(wskey, url, recordType, profile);
+      throw new ApiLimitException(wskey, apiCall, e.getMessage(), requestNumber, 401);
+    } catch (LimitReachedException e) {
+      apiLogService.logApiRequest(wskey, url, RecordType.LIMIT, recordType.toString());
+      throw new ApiLimitException(wskey, apiCall, e.getMessage(), requestNumber, 429);
+    }
+    return new LimitResponse(apiKey, requestNumber);
+  }
 
-	public void addResponseHeaders(HttpServletResponse response) {
-		addCharacterEncoding(response);
-		addAccessControlHeaders(response);
-	}
+  /**
+   * Bundling method for adding both {@link ControllerUtils#addCharacterEncoding character encoding}
+   * and {@link ControllerUtils#addAccessControlHeaders access control headers} to the response with one call
+   * 
+   * @param response The response to add the encoding and headers to
+   */
+  public void addResponseHeaders(HttpServletResponse response) {
+    addCharacterEncoding(response);
+    addAccessControlHeaders(response);
+  }
 
-	public void addCharacterEncoding(HttpServletResponse response) {
-		response.setCharacterEncoding("UTF-8");
-	}
+  /**
+   * Add the 'UTF-8' character encoding to the response
+   * @param response The response to add the character encoding to
+   */
+  public void addCharacterEncoding(HttpServletResponse response) {
+    response.setCharacterEncoding("UTF-8");
+  }
 
-	public void addAccessControlHeaders(HttpServletResponse response) {
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		response.addHeader("Access-Control-Allow-Methods", "POST");
-		response.addHeader("Access-Control-Max-Age", "1000");
-	}
+  /**
+   * Add the access control headers to the response, allowing origin '*', methods 'POST' and max age '1000'
+   * @param response The response to add access control headers to
+   */
+  public void addAccessControlHeaders(HttpServletResponse response) {
+    response.addHeader("Access-Control-Allow-Origin", "*");
+    response.addHeader("Access-Control-Allow-Methods", "POST");
+    response.addHeader("Access-Control-Max-Age", "1000");
+  }
 }
