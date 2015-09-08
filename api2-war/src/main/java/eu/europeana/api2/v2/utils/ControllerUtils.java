8948bd7db2097bf624bd8530f0c14d143ec09952
Luthien-in-edhil <maike.dulk@europeana.eu>
2015-09-08 16:40:11 +0200

    merged code for feat_212: made responses conform to latest comments

--- api2-war/src/main/java/eu/europeana/api2/v2/utils/ControllerUtils.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/utils/ControllerUtils.java
@@ -39,18 +39,18 @@ public class ControllerUtils {
    *         daily limit has been reached
    */
   public LimitResponse checkLimit(String wskey, String url, String apiCall, RecordType recordType,
-      String profile) throws ApiLimitException {
+                                  String profile) throws ApiLimitException {
     ApiKey apiKey = null;
     long requestNumber = 0;
     long t;
     if (wskey == null || "".equalsIgnoreCase(wskey)){
-      throw new ApiLimitException(wskey, apiCall, "no API key provided", 0, 401);
+      throw new ApiLimitException(wskey, apiCall, "no API key provided", 0, 403);
     }
     try {
       t = System.currentTimeMillis();
       apiKey = apiService.findByID(wskey);
       if (apiKey == null) {
-        throw new ApiLimitException(wskey, apiCall, "Unregistered user", 0, 401);
+        throw new ApiLimitException(wskey, apiCall, "Invalid API key", 0, 403);
       }
 //       apiKey.getUsageLimit();
       log.info("get apiKey: " + (System.currentTimeMillis() - t));
