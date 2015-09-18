0d03c8d82d13df06390d75fa68de9c43072bb2e0
Paul Sabou <paul.sabou@gmail.com>
2015-09-18 18:45:10 +0200

    removed several log statements & factored out all fake tags calculations to corelib

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/ContentReuseFrameworkController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/ContentReuseFrameworkController.java
@@ -1,29 +1,6 @@
 package eu.europeana.api2.v2.web.controller;
 
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import javax.annotation.Resource;
-import javax.imageio.ImageIO;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import eu.europeana.api2.ApiLimitException;
-import eu.europeana.corelib.service.MediaStorageClient;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-
 import eu.europeana.api2.model.json.ApiError;
 import eu.europeana.api2.utils.JsonUtils;
 import eu.europeana.api2.v2.model.LimitResponse;
@@ -39,10 +16,30 @@ import eu.europeana.corelib.domain.MediaFile;
 import eu.europeana.corelib.logging.Log;
 import eu.europeana.corelib.logging.Logger;
 import eu.europeana.corelib.search.SearchService;
-import eu.europeana.corelib.service.impl.MediaStorageClientImpl;
 import eu.europeana.corelib.web.service.ContentReuseFrameworkService;
 import eu.europeana.corelib.web.service.EuropeanaUrlService;
+import eu.europeana.corelib.web.service.MediaStorageService;
 import eu.europeana.harvester.domain.SourceDocumentReferenceMetaInfo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 @Controller
 public class ContentReuseFrameworkController {
@@ -53,11 +50,11 @@ public class ContentReuseFrameworkController {
     @Resource
     private SearchService searchService;
 
-    @Resource(name = "corelib_web_contentReuseFrameworkService")
+    @Resource
     private ContentReuseFrameworkService crfService;
 
-    @Resource(name = "corelib_mediaStorageClient")
-    private MediaStorageClient mediaStorageClient;
+    @Resource
+    private MediaStorageService mediaStorageService;
 
     @Resource
     private ApiLogService apiLogService;
@@ -109,11 +106,7 @@ public class ContentReuseFrameworkController {
                                                         HttpServletResponse response) {
         controllerUtils.addResponseHeaders(response);
 
-        log.info("Thumbnail");
-
         String sufix = "";
-        log.info("Size: " + size);
-        log.info("Type: " + type);
 
         if(size.equalsIgnoreCase("BRIEF_DOC") || size.equalsIgnoreCase("h180") ||
            size.equalsIgnoreCase("FULL_DOC") || size.equalsIgnoreCase("FULL_DOC_ALL") || size.equalsIgnoreCase("w200")) {
@@ -128,8 +121,7 @@ public class ContentReuseFrameworkController {
         byte[] imageResponse = DefaultImage.getImage(ThumbSize.LARGE, DocType.IMAGE);
         if(type.equalsIgnoreCase("IMAGE")) {
             final String ID = getMD5(url) + "-" + ("200".equals(sufix) ? "MEDIUM" : "LARGE");
-            log.info("Url: " + url + "  Image: " + ID + " sufix: " + ("200".equals(sufix) ? "MEDIUM" : "LARGE"));
-            final MediaFile mediaFile = mediaStorageClient.retrieve(ID, true);
+            final MediaFile mediaFile = mediaStorageService.retrieve(ID, true);
 
             if (mediaFile != null) {
                 if (mediaFile.getContentType().equals("image/jpeg")) {
