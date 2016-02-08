8b61853103f263ffc1748594df8ddd3d7b9b314e
Willem-Jan Boogerd <wjboogerd@users.noreply.github.com>
2016-02-08 17:07:39 +0100

    Preperations for reconfigure web mvc structure

--- api2-war/src/main/java/eu/europeana/api2/config/WebMvcConfig.java
+++ api2-war/src/main/java/eu/europeana/api2/config/WebMvcConfig.java
@@ -1,5 +1,8 @@
 package eu.europeana.api2.config;
 
+import eu.europeana.api2.config.viewresolver.Jaxb2MarshallingXmlViewResolver;
+import eu.europeana.api2.config.viewresolver.JsonViewResolver;
+import eu.europeana.api2.config.viewresolver.JspViewResolver;
 import eu.europeana.api2.utils.XmlUtils;
 import eu.europeana.api2.v2.model.xml.kml.KmlResponse;
 import eu.europeana.api2.v2.model.xml.rss.RssResponse;
@@ -12,18 +15,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.web.accept.ContentNegotiationManagerFactoryBean;
-import org.springframework.web.servlet.View;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 import org.springframework.web.servlet.view.xml.MarshallingView;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 /**
  * @author Willem-Jan Boogerd (www.eledge.net/contact).
@@ -34,32 +38,69 @@ import java.util.Collections;
 @Import(SwaggerConfig.class)
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-//    @Bean
-//    public ViewResolver contentViewResolver() throws Exception {
-//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//        viewResolver.setPrefix("/WEB-INF/jsp/");
-//        viewResolver.setSuffix(".jsp");
-//        return viewResolver;
-//    }
-
     @Bean
-    public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
-        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
-        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
-
+    public ViewResolver contentViewResolver() throws Exception {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
         viewResolver.setPrefix("/WEB-INF/jsp/");
         viewResolver.setSuffix(".jsp");
-
-        MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
-        defaultView.setExtractValueFromSingleKeyModel(true);
-
-        ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
-        contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
-        contentViewResolver.setViewResolvers(Collections.<ViewResolver>singletonList(viewResolver));
-        contentViewResolver.setDefaultViews(Collections.<View>singletonList(defaultView));
-        return contentViewResolver;
+        return viewResolver;
     }
+
+//    @Bean
+//    public ContentNegotiatingViewResolver contentViewResolver() throws Exception {
+//        ContentNegotiationManagerFactoryBean contentNegotiationManager = new ContentNegotiationManagerFactoryBean();
+//        contentNegotiationManager.addMediaType("json", MediaType.APPLICATION_JSON);
+//
+//        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+//        viewResolver.setPrefix("/WEB-INF/jsp/");
+//        viewResolver.setSuffix(".jsp");
+//
+//        MappingJackson2JsonView defaultView = new MappingJackson2JsonView();
+//        defaultView.setExtractValueFromSingleKeyModel(true);
+//
+//        ContentNegotiatingViewResolver contentViewResolver = new ContentNegotiatingViewResolver();
+//        contentViewResolver.setContentNegotiationManager(contentNegotiationManager.getObject());
+//        contentViewResolver.setViewResolvers(Collections.<ViewResolver>singletonList(viewResolver));
+//        contentViewResolver.setDefaultViews(Collections.<View>singletonList(defaultView));
+//        return contentViewResolver;
+//    }
+
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//        configurer.ignoreAcceptHeader(true).defaultContentType(
+//                MediaType.TEXT_HTML);
+//    }
+//
+//    @Bean
+//    public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+//        ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+//        resolver.setContentNegotiationManager(manager);
+//
+//        // Define all possible view resolvers
+//        List<ViewResolver> resolvers = new ArrayList<>();
+//
+//        resolvers.add(jaxb2MarshallingXmlViewResolver());
+////        resolvers.add(jsonViewResolver());
+//        resolvers.add(jspViewResolver());
+//
+//        resolver.setViewResolvers(resolvers);
+//        return resolver;
+//    }
+
+//    @Bean
+//    public ViewResolver jaxb2MarshallingXmlViewResolver() {
+//        return new Jaxb2MarshallingXmlViewResolver(jaxb2Marshaller());
+//    }
+
+//    @Bean
+//    public ViewResolver jsonViewResolver() {
+//        return new JsonViewResolver();
+//    }
+
+//    @Bean
+//    public ViewResolver jspViewResolver() {
+//        return new JspViewResolver();
+//    }
 
     @Bean(name = "api2_mvc_views_jaxbmarshaller")
     public Jaxb2Marshaller jaxb2Marshaller() {
