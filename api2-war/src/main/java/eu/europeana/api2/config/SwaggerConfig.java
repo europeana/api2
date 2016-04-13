d56dcd8bf65766b7b0f6fd89ba5a39840fa493c5
Luthien-in-edhil <maike.dulk@europeana.eu>
2016-04-13 10:45:52 +0200

    initial commit

--- api2-war/src/main/java/eu/europeana/api2/config/SwaggerConfig.java
+++ api2-war/src/main/java/eu/europeana/api2/config/SwaggerConfig.java
@@ -65,7 +65,7 @@ public class SwaggerConfig {
         return new ApiInfo(
         "Europeana REST API",
         "This Swagger API console provides an overview of and interface to the Europeana REST API. " +
-                "You can build and test anything from the simplest search to a complex query using facets " +
+                "You can build and test anything from the simplest search to a complex query using facetList " +
                 "such as dates, geotags and permissions. For more help and information, head to our " +
                 "comprehensive <a href=\"http://labs.europeana.api/api/\">online documentation</a>.",
         "2.0.14",
