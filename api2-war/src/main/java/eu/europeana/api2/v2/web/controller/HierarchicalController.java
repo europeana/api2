7923ee9c3cddbbeeacfa634a2f182c8d8f221457
Luthien-in-edhil <maike.dulk@europeana.eu>
2016-11-28 14:17:39 +0100

    cleanup hierarchicalcontroller

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/HierarchicalController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/HierarchicalController.java
@@ -17,27 +17,18 @@
 
 package eu.europeana.api2.v2.web.controller;
 
-import eu.europeana.api2.ApiLimitException;
 import eu.europeana.api2.model.json.ApiError;
 import eu.europeana.api2.utils.JsonUtils;
-import eu.europeana.api2.v2.model.LimitResponse;
-import eu.europeana.api2.v2.model.json.HierarchicalResult;
 import eu.europeana.api2.v2.service.HierarchyRunner;
 import eu.europeana.api2.v2.utils.ControllerUtils;
 import eu.europeana.api2.v2.web.swagger.SwaggerIgnore;
 import eu.europeana.corelib.db.entity.enums.RecordType;
 import eu.europeana.corelib.definitions.exception.Neo4JException;
-import eu.europeana.corelib.neo4j.entity.Neo4jBean;
-import eu.europeana.corelib.neo4j.entity.Neo4jStructBean;
 import eu.europeana.corelib.search.SearchService;
-import eu.europeana.corelib.web.utils.RequestUtils;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,9 +40,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * @author Willem-Jan Boogerd <www.eledge.net/contact>
@@ -64,7 +54,6 @@ import java.util.concurrent.*;
 public class HierarchicalController {
 
     private static Logger log = Logger.getLogger(HierarchicalController.class);
-    private static final int MAX_LIMIT = 100;
 
     @Resource
     private SearchService searchService;
@@ -72,10 +61,6 @@ public class HierarchicalController {
     @Resource
     private ControllerUtils controllerUtils;
 
-    @Resource
-    private ObjectController objectController;
-
-//    @SuppressWarnings("SpringJavaAutowiringInspection")
     @Bean
     public HierarchyRunner hierarchyRunnerBean() {
         return new HierarchyRunner();
@@ -149,29 +134,12 @@ public class HierarchicalController {
                 profile, wskey, limit, offset, callback, request, response, redirectAttrs);
     }
 
+    // maintain backwards compatibility with previous spelling of "preceeding"
     @ApiOperation(value = "returns the object's preceding siblings")
-    @RequestMapping(value = "/{collectionId}/{recordId}/preceding-siblings.json", method = RequestMethod.GET,
+    @RequestMapping(value = {"/{collectionId}/{recordId}/preceding-siblings.json",
+            "/{collectionId}/{recordId}/preceeding-siblings.json"}, method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ModelAndView getPrecedingSiblings(
-            @PathVariable String collectionId,
-            @PathVariable String recordId,
-            @RequestParam(value = "profile", required = false, defaultValue = "") String profile,
-            @RequestParam(value = "wskey", required = true) String wskey,
-            @RequestParam(value = "limit", required = true, defaultValue = "10") int limit,
-            @RequestParam(value = "offset", required = true, defaultValue = "0") int offset,
-            @RequestParam(value = "callback", required = false) String callback,
-            HttpServletRequest request,
-            HttpServletResponse response,
-            RedirectAttributes redirectAttrs) {
-        return hierarchyTemplate(RecordType.HIERARCHY_PRECEDING_SIBLINGS, collectionId, recordId,
-                profile, wskey, limit, offset, callback, request, response, redirectAttrs);
-    }
-
-    @SwaggerIgnore
-//    @ApiOperation(value = "returns the object's preceeeeeding siblings (backwards compatibility")
-    @RequestMapping(value = "/{collectionId}/{recordId}/preceeding-siblings.json", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ModelAndView getPreceedingSiblings(
             @PathVariable String collectionId,
             @PathVariable String recordId,
             @RequestParam(value = "profile", required = false, defaultValue = "") String profile,
@@ -204,9 +172,6 @@ public class HierarchicalController {
                 profile, wskey, limit, offset, callback, request, response, redirectAttrs);
     }
 
-    public HierarchicalController() {
-    }
-
 
     public ModelAndView hierarchyTemplate(RecordType recordType, String collectionId, String recordId,
                                           String profile, String wskey, int limit, int offset, String callback,
@@ -222,11 +187,11 @@ public class HierarchicalController {
         } catch (Neo4JException e) {
             log.error("Neo4JException thrown: " + e.getMessage());
             log.error("Cause: " + e.getCause());
-            return generateErrorHierarchy(recordType, rdfAbout, wskey, callback, "Neo4JException");
+            return generateErrorHierarchy(rdfAbout, wskey, callback, "Neo4JException");
         } catch (InterruptedException e) {
             log.error("InterruptedException thrown: " + e.getMessage());
             log.error("Cause: " + e.getCause());
-            return generateErrorHierarchy(recordType, rdfAbout, wskey, callback, "InterruptedException");
+            return generateErrorHierarchy(rdfAbout, wskey, callback, "InterruptedException");
         } catch (ExecutionException e) {
             log.error("ExecutionExeption thrown: " + e.getMessage());
             log.error("Cause: " + e.getCause());
@@ -238,29 +203,11 @@ public class HierarchicalController {
         }
     }
 
-    private ModelAndView generateErrorHierarchy(RecordType recordType, String rdfAbout, String wskey, String callback,
+    private ModelAndView generateErrorHierarchy(String rdfAbout, String wskey, String callback,
                                                 String exceptionType) {
         return JsonUtils.toJson(new ApiError(wskey, String.format(exceptionType + " thrown when processing record %s",
                 rdfAbout), -1L), callback);
 
-    }
-
-    protected static String getAction(RecordType recordType) {
-        String action = "";
-        if (recordType.equals(RecordType.HIERARCHY_CHILDREN)) {
-            action = "children.json";
-        } else if (recordType.equals(RecordType.HIERARCHY_SELF)) {
-            action = "self.json";
-        } else if (recordType.equals(RecordType.HIERARCHY_PARENT)) {
-            action = "parent.json";
-        } else if (recordType.equals(RecordType.HIERARCHY_FOLLOWING_SIBLINGS)) {
-            action = "following-siblings.json";
-        } else if (recordType.equals(RecordType.HIERARCHY_PRECEDING_SIBLINGS)) {
-            action = "preceding-siblings.json";
-        } else if (recordType.equals(RecordType.HIERARCHY_ANCESTOR_SELF_SIBLINGS)) {
-            action = "ancestor-self-siblings.json";
-        }
-        return action;
     }
 
 }
