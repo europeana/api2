baf4e73d2700e293ab4840ffbe8f7c3d71b1d21a
Luthien-in-edhil <maike.dulk@europeana.eu>
2017-05-03 14:00:43 +0200

    restored #411 edits & updated version to 2.6.2-SNAPSHOT

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/SearchController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/SearchController.java
@@ -350,7 +350,9 @@ public class SearchController {
 
         // create Query object and set some params
         Class<? extends IdBean> clazz = selectBean(profile);
-        Query query = new Query(SearchUtils.rewriteQueryFields(queryString))
+//        Query query = new Query(SearchUtils.rewriteQueryFields(queryString))
+        Query query = new Query(SearchUtils.rewriteQueryFields(
+                                SearchUtils.fixBuggySolrIndex(queryString)))
                 .setApiQuery(true)
                 .setRefinements(refinementArray)
                 .setPageSize(rows)
