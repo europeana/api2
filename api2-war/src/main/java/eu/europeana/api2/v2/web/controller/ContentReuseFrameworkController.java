626fc5b95539aa04aaa7a2b47e41bf85f2cd58c3
Patrick Ehlert <patrick.ehlert@europeana.eu>
2017-05-08 12:06:49 +0200

    Add documentation and clean-up code (fix sonar issues)

--- api2-war/src/main/java/eu/europeana/api2/v2/web/controller/ContentReuseFrameworkController.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/web/controller/ContentReuseFrameworkController.java
@@ -21,6 +21,7 @@ import eu.europeana.api2.v2.utils.ControllerUtils;
 import eu.europeana.corelib.domain.MediaFile;
 import eu.europeana.corelib.web.service.MediaStorageService;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -33,15 +34,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.annotation.Resource;
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Retrieves image thumbnails.
+ * The thumbnail API doesn't require any form of authentication, providing an API key is optional.
+ */
 @Controller
 public class ContentReuseFrameworkController {
+
+    private static final Logger LOG = Logger.getLogger(ContentReuseFrameworkController.class);
 
     @Resource
     private MediaStorageService mediaStorageService;
@@ -49,6 +55,16 @@ public class ContentReuseFrameworkController {
     @Resource
     private ControllerUtils controllerUtils;
 
+    /**
+     * Retrieves image thumbnails.
+     * @param url optional, the URL of the media resource of which a thumbnail should be returned. Note that the URL should be encoded.
+     *            When no url is provided a default thumbnail will be returned
+     * @param size optional, the size of the thumbnail, can either be w200 (width 200) or w400 (width 400).
+     * @param type optional, type of the default thumbnail (media image) in case the thumbnail does not exists or no url is provided, can be: IMAGE, SOUND, VIDEO, TEXT or 3D.
+     * @param response
+     * @return
+     * @throws IOException
+     */
     @RequestMapping(value = "/v2/thumbnail-by-url.json", method = RequestMethod.GET)
     public ResponseEntity<byte[]> thumbnailByUrl(
             @RequestParam(value = "uri", required = false) String url,
@@ -56,12 +72,11 @@ public class ContentReuseFrameworkController {
             @RequestParam(value = "type", required = false, defaultValue = "IMAGE") String type,
             HttpServletResponse response) throws IOException {
         controllerUtils.addResponseHeaders(response);
-        url = (url == null ? "": url);
         final HttpHeaders headers = new HttpHeaders();
         final String mediaFileId = computeResourceUrl(url, size);
         final MediaFile mediaFile = mediaStorageService.retrieve(mediaFileId, true);
 
-        byte[] mediaResponse = null;
+        byte[] mediaResponse;
         if (mediaFile != null) {
             mediaResponse = mediaFile.getContent();
             // All stored thumbnails are JPEG.
@@ -75,31 +90,38 @@ public class ContentReuseFrameworkController {
         return new ResponseEntity<>(mediaResponse, headers, HttpStatus.OK);
     }
 
-
+    /**
+     * Retrieve the default thumbnail image as a byte array
+     * @param path
+     * @return
+     */
     private byte[] getImage(String path) {
         byte[] response = null;
-
         BufferedImage img;
         try {
             img = ImageIO.read(getClass().getResourceAsStream(path));
             response = getByteArray(img, path.endsWith(".png") ? "png" : "gif");
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Error reading default thumbnail", e);
         }
         return response;
     }
 
+    /**
+     * Convert a bufferedImage to a byte array
+     * @param bufferedImage
+     * @param formatName
+     * @return
+     */
     private byte[] getByteArray(final BufferedImage bufferedImage, String formatName) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        try {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()){
             ImageIO.write(bufferedImage, formatName, baos);
             baos.flush();
+            return baos.toByteArray();
         } catch (IOException e) {
-            e.printStackTrace();
+            LOG.error("Error writing buffered default thumbnail", e);
         }
-
-        return baos.toByteArray();
+        return new byte[0];
     }
 
     private String getMD5(String input) {
@@ -110,12 +132,13 @@ public class ContentReuseFrameworkController {
             messageDigest.reset();
             messageDigest.update(input.getBytes());
             final byte[] resultByte = messageDigest.digest();
-            StringBuffer sb = new StringBuffer();
+            StringBuilder sb = new StringBuilder();
             for (byte aResultByte : resultByte) {
                 sb.append(Integer.toString((aResultByte & 0xff) + 0x100, 16).substring(1));
             }
             temp = sb.toString();
         } catch (NoSuchAlgorithmException e) {
+            LOG.error("Cannot find MD5 algorithm", e);
             temp = input;
         }
 
@@ -139,8 +162,15 @@ public class ContentReuseFrameworkController {
         }
     }
 
+    /**
+     * Convert the provided url and size into a string representing the id of the media file.
+     * @param resourceUrl
+     * @param resourceSize
+     * @return
+     */
     private String computeResourceUrl(final String resourceUrl, final String resourceSize) {
-        return getMD5(resourceUrl) + "-" + (StringUtils.equalsIgnoreCase(resourceSize, "w400") ? "LARGE" : "MEDIUM");
+        String urlText = (resourceUrl == null ? "" : resourceUrl);
+        return getMD5(urlText) + "-" + (StringUtils.equalsIgnoreCase(resourceSize, "w200") ? "MEDIUM" : "LARGE");
 
     }
 }
