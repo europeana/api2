afef6bdd6aff35f20311e95842f5332e67f95d22
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-09 13:47:07 +0200

    some remaining clean-up

--- api2-war/src/main/java/eu/europeana/api2/web/controller/AccessConfirmationController.java
+++ api2-war/src/main/java/eu/europeana/api2/web/controller/AccessConfirmationController.java
@@ -40,51 +40,51 @@ import java.util.Map;
  * Controller for retrieving the model for and displaying the confirmation page for access to a protected resource.
  * @deprecated 2018-01-09 old MyEuropeana functionality*
  */
-@Controller
-@SessionAttributes("authorizationRequest")
-@Deprecated
+//@Controller
+//@SessionAttributes("authorizationRequest")
+//@Deprecated
 public class AccessConfirmationController {
 
-    @Resource
-    private ApiKeyService apiKeyService;
-
-    @Resource
-    private ApprovalStore approvalStore;
-
-    @Resource(name = "api2_oauth2_clientDetailsService")
-    private ClientDetailsService clientDetailsService;
-
-    @RequestMapping("/oauth/confirm_access")
-    public ModelAndView getAccessConfirmation(Map<String, Object> model, Principal principal) throws Exception {
-        AuthorizationRequest clientAuth = (AuthorizationRequest) model.remove("authorizationRequest");
-        ClientDetails client = clientDetailsService.loadClientByClientId(clientAuth.getClientId());
-        ApiKey key = apiKeyService.findByID(client.getClientId());
-        model.put("auth_request", clientAuth);
-        model.put("client", client);
-        model.put("appName", StringUtils.defaultIfBlank(key.getApplicationName(), StringUtils.defaultIfBlank(key.getCompany(), key.getId())));
-        Map<String, String> scopes = new LinkedHashMap<>();
-        for (String scope : clientAuth.getScope()) {
-            scopes.put(OAuth2Utils.SCOPE_PREFIX + scope, "false");
-        }
-        approvalStore.getApprovals(principal.getName(), client.getClientId()).stream()
-                .filter(approval -> clientAuth.getScope().contains(approval.getScope()))
-                .forEach(approval -> scopes.put(OAuth2Utils.SCOPE_PREFIX + approval.getScope(),
-                        approval.getStatus() == Approval.ApprovalStatus.APPROVED ? "true" : "false"));
-        model.put("scopes", scopes);
-        return new ModelAndView("user/authorize", model);
-    }
-
-    @RequestMapping("/oauth/error")
-    public String handleError(Map<String, Object> model) throws Exception {
-        // We can add more stuff to the model here for JSP rendering. If the client was a machine then
-        // the JSON will already have been rendered.
-        model.put("message", "There was a problem with the OAuth2 protocol");
-        return "user/error";
-    }
-
-    @RequestMapping(value = "/oAuthLogin")
-    public String loginUserForm() {
-        return "user/login";
-    }
+//    @Resource
+//    private ApiKeyService apiKeyService;
+//
+//    @Resource
+//    private ApprovalStore approvalStore;
+//
+//    @Resource(name = "api2_oauth2_clientDetailsService")
+//    private ClientDetailsService clientDetailsService;
+//
+//    @RequestMapping("/oauth/confirm_access")
+//    public ModelAndView getAccessConfirmation(Map<String, Object> model, Principal principal) throws Exception {
+//        AuthorizationRequest clientAuth = (AuthorizationRequest) model.remove("authorizationRequest");
+//        ClientDetails client = clientDetailsService.loadClientByClientId(clientAuth.getClientId());
+//        ApiKey key = apiKeyService.findByID(client.getClientId());
+//        model.put("auth_request", clientAuth);
+//        model.put("client", client);
+//        model.put("appName", StringUtils.defaultIfBlank(key.getApplicationName(), StringUtils.defaultIfBlank(key.getCompany(), key.getId())));
+//        Map<String, String> scopes = new LinkedHashMap<>();
+//        for (String scope : clientAuth.getScope()) {
+//            scopes.put(OAuth2Utils.SCOPE_PREFIX + scope, "false");
+//        }
+//        approvalStore.getApprovals(principal.getName(), client.getClientId()).stream()
+//                .filter(approval -> clientAuth.getScope().contains(approval.getScope()))
+//                .forEach(approval -> scopes.put(OAuth2Utils.SCOPE_PREFIX + approval.getScope(),
+//                        approval.getStatus() == Approval.ApprovalStatus.APPROVED ? "true" : "false"));
+//        model.put("scopes", scopes);
+//        return new ModelAndView("user/authorize", model);
+//    }
+//
+//    @RequestMapping("/oauth/error")
+//    public String handleError(Map<String, Object> model) throws Exception {
+//        // We can add more stuff to the model here for JSP rendering. If the client was a machine then
+//        // the JSON will already have been rendered.
+//        model.put("message", "There was a problem with the OAuth2 protocol");
+//        return "user/error";
+//    }
+//
+//    @RequestMapping(value = "/oAuthLogin")
+//    public String loginUserForm() {
+//        return "user/login";
+//    }
 
 }
