f27c66c715ac2583739772da74f70038941f3ba5
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-22 13:20:00 +0200

    updated license.md and removed all license headers from source files

--- api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/fieldtrip/package-info.java
+++ api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/fieldtrip/package-info.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2007-2015 The Europeana Foundation
- *
- * Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
- * by the European Commission;
- * You may not use this work except in compliance with the Licence.
- *
- * You may obtain a copy of the Licence at:
- * http://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the Licence is distributed on an "AS IS" basis, without warranties or conditions of
- * any kind, either express or implied.
- * See the Licence for the specific language governing permissions and limitations under
- * the Licence.
- */
+
 
 @XmlSchema(
 	namespace = "",
@@ -22,8 +7,7 @@
 		@XmlNs(namespaceURI = "http://www.georss.org/georss", prefix = "georss"),
 	},
 	elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-package eu.europeana.api2.v2.model.xml.rss.fieldtrip;
+)package eu.europeana.api2.v2.model.xml.rss.fieldtrip;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
--- api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/package-info.java
+++ api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/package-info.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2007-2015 The Europeana Foundation
- *
- * Licenced under the EUPL, Version 1.1 (the "Licence") and subsequent versions as approved
- * by the European Commission;
- * You may not use this work except in compliance with the Licence.
- *
- * You may obtain a copy of the Licence at:
- * http://joinup.ec.europa.eu/software/page/eupl
- *
- * Unless required by applicable law or agreed to in writing, software distributed under
- * the Licence is distributed on an "AS IS" basis, without warranties or conditions of
- * any kind, either express or implied.
- * See the Licence for the specific language governing permissions and limitations under
- * the Licence.
- */
+
 
 @XmlSchema(namespace = "", xmlns = {
 		@XmlNs(namespaceURI = "http://a9.com/-/spec/opensearch/1.1/", prefix = "opensearch"),
@@ -23,8 +8,7 @@
 		@XmlNs(namespaceURI = "http://www.fieldtripper.com/fieldtrip_rss", prefix = "fieldtrip"),
 		@XmlNs(namespaceURI = "http://www.georss.org/georss", prefix = "georss"),
 		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL, prefix = "europeana"),
-		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL +"/schemas/ese/enrichment/", prefix = "enrichment") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package eu.europeana.api2.v2.model.xml.rss;
+		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL +"/schemas/ese/enrichment/", prefix = "enrichment") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)package eu.europeana.api2.v2.model.xml.rss;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
