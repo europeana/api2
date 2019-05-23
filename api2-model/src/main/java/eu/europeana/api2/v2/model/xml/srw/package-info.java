720344e6f5ef50dedac47b92ab7f8707c191fbb3
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-23 11:55:59 +0200

    fixed package-info

--- api2-model/src/main/java/eu/europeana/api2/v2/model/xml/srw/package-info.java
+++ api2-model/src/main/java/eu/europeana/api2/v2/model/xml/srw/package-info.java
@@ -9,7 +9,9 @@
 		@XmlNs(namespaceURI = SrwResponse.NS_TEL, prefix = "tel"),
 		@XmlNs(namespaceURI = SrwResponse.NS_XSI, prefix = "xsi"),
 		@XmlNs(namespaceURI = SrwResponse.NS_DCTERMS, prefix = "dcterms"),
-		@XmlNs(namespaceURI = SrwResponse.NS_DC, prefix = "dc") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)package eu.europeana.api2.v2.model.xml.srw;
+		@XmlNs(namespaceURI = SrwResponse.NS_DC, prefix = "dc") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+
+package eu.europeana.api2.v2.model.xml.srw;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
