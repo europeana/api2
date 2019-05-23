6e44c86b1565427c762deebe996164bf4b9facba
Luthien-in-edhil <maike.dulk@europeana.eu>
2019-05-23 11:54:16 +0200

    fixed package-info

--- api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/package-info.java
+++ api2-model/src/main/java/eu/europeana/api2/v2/model/xml/rss/package-info.java
@@ -6,7 +6,9 @@
 		@XmlNs(namespaceURI = "http://www.fieldtripper.com/fieldtrip_rss", prefix = "fieldtrip"),
 		@XmlNs(namespaceURI = "http://www.georss.org/georss", prefix = "georss"),
 		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL, prefix = "europeana"),
-		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL +"/schemas/ese/enrichment/", prefix = "enrichment") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)package eu.europeana.api2.v2.model.xml.rss;
+		@XmlNs(namespaceURI = EuropeanaStaticUrl.EUROPEANA_NAMESPACE_URL +"/schemas/ese/enrichment/", prefix = "enrichment") }, elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+
+package eu.europeana.api2.v2.model.xml.rss;
 
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlSchema;
