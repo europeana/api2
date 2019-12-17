7ce588fa92456a81f1ba110c6837302a28c13c2b
srishti <srishti.singh@europeana.eu>
2019-12-17 11:02:30 +0100

    EA-1818 added Junit test

--- api2-war/src/test/java/eu/europeana/api2/v2/web/controller/ObjectControllerTest.java
+++ api2-war/src/test/java/eu/europeana/api2/v2/web/controller/ObjectControllerTest.java
@@ -2,35 +2,21 @@ package eu.europeana.api2.v2.web.controller;
 
 import eu.europeana.api2.v2.utils.ApiKeyUtils;
 import eu.europeana.api2.v2.utils.HttpCacheUtils;
-import eu.europeana.corelib.definitions.edm.beans.FullBean;
 import eu.europeana.corelib.search.SearchService;
-import eu.europeana.corelib.solr.bean.impl.FullBeanImpl;
 import org.junit.Before;
 
-import static org.mockito.Matchers.anyBoolean;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-
 import org.junit.Test;
-import org.mockito.Matchers;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.util.NestedServletException;
 
 public class ObjectControllerTest {
 
     private static final String MEDIA_TYPE_JSONLD_UTF8  = "application/ld+json; charset=UTF-8";
-    private static final String COLLECTIONID_TEST = "2024904";
-    private static final String RECORDID_TEST  = "photography_ProvidedCHO_TopFoto_co_uk_EU017407";
-
-
-    private static FullBean bean = new FullBeanImpl();
 
     private static ObjectController objectController;
     private static SearchService searchService;
@@ -45,11 +31,13 @@ public class ObjectControllerTest {
         searchService = mock(SearchService.class);
         apiKeyUtils = mock(ApiKeyUtils.class);
         httpCacheUtils = mock(HttpCacheUtils.class);
-        objectController = new ObjectController(searchService, apiKeyUtils, httpCacheUtils);
+
+        objectController = spy(new ObjectController(searchService, apiKeyUtils, httpCacheUtils));
 
         objectControllerMock = MockMvcBuilders
                 .standaloneSetup(objectController)
                 .build();
+
     }
 
     @Test
@@ -65,52 +53,10 @@ public class ObjectControllerTest {
         objectControllerMock.perform(get("/v2/record/context.jsonld")
                 .accept("*"))
                 .andExpect(status().is(200));
+
+        //Invalid accept Header
+        objectControllerMock.perform(get("/v2/record/context.jsonld")
+                .accept(MediaType.APPLICATION_XML))
+                .andExpect(status().is(406));
     }
-
-    @Test(expected= NestedServletException.class)
-    public void recordJSON_LDAcceptHeaderTest () throws Exception {
-        FullBean bean = new FullBeanImpl();
-
-        when(searchService.fetchFullBean(anyString())).thenReturn(bean);
-        when(searchService.processFullBean(Matchers.any(), Matchers.anyString(), anyBoolean())).thenReturn(bean);
-
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.jsonld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.jsonld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept(MEDIA_TYPE_JSONLD_UTF8))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.jsonld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept("*"))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.json-ld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.json-ld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept(MEDIA_TYPE_JSONLD_UTF8))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-        objectControllerMock.perform(get("/v2/record/{collectionId}/{recordId}.json-ld" , COLLECTIONID_TEST, RECORDID_TEST)
-                .param("wskey", "api2demo")
-                .accept("*"))
-                .andExpect(status().is(200))
-                .andExpect(content().contentType(MEDIA_TYPE_JSONLD_UTF8));
-
-    }
-
 }
