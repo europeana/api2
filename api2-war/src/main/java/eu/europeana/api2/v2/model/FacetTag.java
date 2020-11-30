d1f005a3ad6775f94e23eeddd4c0a5eb704793e1
Luthien-in-edhil <luthien@parendili.org>
2020-11-30 12:18:02 +0100

    hotfix for EA-2344 lowercase colourpalette facet labels

--- api2-war/src/main/java/eu/europeana/api2/v2/model/FacetTag.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/model/FacetTag.java
@@ -1,5 +1,6 @@
 package eu.europeana.api2.v2.model;
 
+import eu.europeana.corelib.definitions.solr.TechnicalFacetType;
 import org.apache.commons.lang3.StringUtils;
 
 /**
@@ -26,9 +27,11 @@ public class FacetTag {
             return "";
         }
     }
-
+    
     public String getLabel() {
-        if (label != null) {
+        if ((StringUtils.isNotBlank(label)) && StringUtils.equals(name, TechnicalFacetType.COLOURPALETTE.getRealName())) {
+            return translateMetisTerms(label);
+        } else if (StringUtils.isNotBlank(label)) {
             return translateMetisTerms(StringUtils.lowerCase(label));
         } else {
             return "";
