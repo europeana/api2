bbb34762aeee7db04ead8ca32f568fcb49e026a8
Luthien-in-edhil <luthien@parendili.org>
2023-05-30 15:14:18 +0200

    configured the Swagger feed for production

--- api2-war/src/main/webapp/console/swagger-initializer.js
+++ api2-war/src/main/webapp/console/swagger-initializer.js
@@ -4,7 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "https://petstore.swagger.io/v2/swagger.json",
-    url: "https://search-api.test.eanadev.org/api/api-docs",
+    //url: "https://search-api.test.eanadev.org/api/api-docs",
+    url: "https://api.europeana.eu/api/api-docs",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
