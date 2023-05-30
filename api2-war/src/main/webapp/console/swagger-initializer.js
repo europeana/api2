c9773066605eef65911cfc9f945133063814f005
Luthien-in-edhil <luthien@parendili.org>
2023-05-30 15:04:42 +0200

    set feed link in sagger-initializr.js as well

--- api2-war/src/main/webapp/console/swagger-initializer.js
+++ api2-war/src/main/webapp/console/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     // url: "https://petstore.swagger.io/v2/swagger.json",
-    url: "https://search-api2.test.eanadev.org/api/api-docs",
+    url: "https://search-api.test.eanadev.org/api/api-docs",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
