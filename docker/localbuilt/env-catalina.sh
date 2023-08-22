71726d97596e03667ad5212da8c668b3d56c4074
Patrick Ehlert <patrick.ehlert@europeana.eu>
2023-08-22 16:48:09 +0200

    Delete old docker local image build

--- docker/localbuilt/env-catalina.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-# Script to generate europeana.properties file in Docker container from europeana.env.properties file
-envsubst < webapps/api/europeana.env.properties > webapps/api/WEB-INF/classes/europeana.properties
-rm webapps/api/europeana.env.properties
-exec "$@"
