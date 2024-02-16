0802f8fa84da544f2571f83bdbd6ee3a4505d960
Shwetambara Nazare <nshweta90@gmail.com>
2024-02-16 18:51:39 +0100

    Changes for parser implementation

--- query-syntax/src/test/java/eu/europeana/api/search/syntax/TestSearchParser.java
+++ query-syntax/src/test/java/eu/europeana/api/search/syntax/TestSearchParser.java
@@ -83,4 +83,11 @@ public class TestSearchParser {
 
   }
 
+
+
+  public void testFieldSearchQuery_when_AND_OR_Operator() throws  ParseException {
+    Assert.assertEquals("issued_date:1950 AND issued_date:",ParserUtils.parseQueryFilter("issued:1950 AND issued_date:1950"));
+
+  }
+
 }
