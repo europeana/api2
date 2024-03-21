09d78b3f46e94a08f75dc18642ff70360c35266f
Shwetambara Nazare <nshweta90@gmail.com>
2024-03-21 16:35:07 +0100

    Changes for parser implementation

--- query-syntax/src/main/java/eu/europeana/api/search/syntax/function/DistanceFunction.java
+++ query-syntax/src/main/java/eu/europeana/api/search/syntax/function/DistanceFunction.java
@@ -2,6 +2,7 @@ package eu.europeana.api.search.syntax.function;
 
 import static eu.europeana.api.search.syntax.validation.SyntaxErrorUtils.newException;
 import static eu.europeana.api.search.syntax.validation.SyntaxErrorUtils.newWrongFunctionArg;
+import static java.lang.Math.abs;
 
 import eu.europeana.api.search.syntax.converter.ConverterContext;
 import eu.europeana.api.search.syntax.field.FieldMode;
@@ -38,24 +39,37 @@ public class DistanceFunction implements FunctionClass {
   public String toSolr(FunctionExpression expr, ConverterContext context) {
     List<ArgumentExpression> params = expr.getParameters();
     expr.validateArgs();
-    String fieldName = getGeoField(params.get(0), context);
-    context.setParameter(Constants.SFIELD_PARAM, (fieldName + Constants.LOCATION_SUFFIX));
-    context.setParameter(Constants.PT_PARAM,
-        getFloatValue(params.get(1), ArgumentExpression.ARG2) + "," + getFloatValue(params.get(2),
-            ArgumentExpression.ARG3));
-    context.setParameter(Constants.D_PARAM, getFloatValue(params.get(3), ArgumentExpression.ARG4));
+    updateGeoFieldName(context, params);
+    updateLatitudeAndLongitude(context, params);
+    updateDistance(context, params);
     return SOLR_GEODISTANCE_QUERY;
   }
 
-  private String getFloatValue(ArgumentExpression argument, String arg) {
+  private void updateGeoFieldName(ConverterContext context, List<ArgumentExpression> params) {
+    String fieldName = getGeoField(params.get(0), context);
+    context.setParameter(Constants.SFIELD_PARAM, (fieldName + Constants.LOCATION_SUFFIX));
+  }
+
+  private void updateDistance(ConverterContext context, List<ArgumentExpression> params) {
+    Float distance = getFloatValue(params.get(3));
+    if(distance <0 || distance >24000.0) newWrongFunctionArg(this.getName(),ArgumentExpression.ARG4,"positive decimal number which is less than possible distance between two points on Earth (20.004 km)");
+    context.setParameter(Constants.D_PARAM, String.valueOf(distance));
+  }
+
+  private void updateLatitudeAndLongitude(ConverterContext context, List<ArgumentExpression> params) {
+    Float latitude = getFloatValue(params.get(1));
+    if(abs(latitude) > 90.0) newWrongFunctionArg(this.getName(),ArgumentExpression.ARG2," value for latitude must be between -90.0 and 90.0");
+    Float longitude = getFloatValue(params.get(2));
+    if(abs(longitude) > 180.0) newWrongFunctionArg(this.getName(),ArgumentExpression.ARG3," value for longitude must be between -180.0 and 180.0");
+    context.setParameter(Constants.PT_PARAM, latitude + "," + longitude);
+  }
+
+  private Float getFloatValue(ArgumentExpression argument) {
     if ( !(argument instanceof ValueExpression)){
       newException(String.format("Unable to get float value ! Expected Type: %s","ValueExpression"));
     }
     String value = ((ValueExpression) argument).getValue();
-    float f = Float.parseFloat(value);
-    if(f<0)
-       newWrongFunctionArg(this.getName(),arg," Positive Decimal number");
-    return String.valueOf(f);
+    return Float.parseFloat(value);
   }
 
   private String getGeoField(ArgumentExpression arg, ConverterContext context) {
