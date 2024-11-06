269565f1238061196d73b8152c635f6c9fa4da53
Shwetambara Nazare <nshweta90@gmail.com>
2024-11-06 10:14:45 +0100

    gap value issue fix

--- api2-war/src/main/java/eu/europeana/api2/v2/utils/FacetParameterUtils.java
+++ api2-war/src/main/java/eu/europeana/api2/v2/utils/FacetParameterUtils.java
@@ -221,7 +221,7 @@ public class FacetParameterUtils {
                 gapValue = StringUtils.chop(gap);
                 gapValueUnit = dateTimeSpecifiersMap.get(gap.substring(gap.length() - 1));
                 if(gapValueUnit == null)
-                    throw new DataFormatException("Invalid gap value "+gapValue );
+                    throw new DataFormatException("Invalid gap value "+dateRangeParams.get("f." + field + "." + FACET_RANGE_GAP) );
                 gap = gapValue+gapValueUnit;
                 endDate = adjustEndDateForFaceting(endDate,gapValue,gapValueUnit);
             } else {
